abfbb818cf795507696b24a85ad18ec6e33016d0
CyraxSputnik <43629243+CyraxSputnik@users.noreply.github.com>
2025-10-05 16:40:48 -0600

    Just a change

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-        <section>
+        <section className="container mx-auto px-4 py-16"   >
             <h1>Foro</h1>
             <p>Utiliza el foro para preguntar o compartir conocimiento</p>
         </section>
